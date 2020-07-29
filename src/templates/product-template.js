dc671e0f9a057ff4646ab48260b26b099f4cde03
Ali Klein <alexandraeklein@yahoo.com>
2020-07-29 10:31:26 -0700

    Single product parallax

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,35 +1,65 @@
-import React from "react"
+import React, { Component } from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "gatsby-image"
+// import Image from "gatsby-image"
+import BackgroundImage from "gatsby-background-image"
 
-const ProductTemplate = ({
-  data: {
-    product: {
-      title,
-      price,
-      image: { fluid },
-      info: { info },
-    },
-  },
-}) => {
-  return (
-    <Layout>
-      <div style={{ textAlign: "center" }}>
-        <Link to="/products">Back to Products</Link>
-        <h1>Single Product: {title}</h1>
-      </div>
-      <section className="single-product">
-        <Image fluid={fluid} alt={title} />
-        <article>
-          <h1>{title}</h1>
-          <h3>${price}</h3>
-          <p>{info}</p>
-          <button>Add to Cart</button>
-        </article>
-      </section>
-    </Layout>
-  )
+export class productTemplate extends Component {
+  state = {
+    offset: 0,
+  }
+
+  parallaxShift = () => {
+    this.setState({
+      offset: window.pageYOffset,
+    })
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.parallaxShift)
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.parallaxShift)
+  }
+
+  render() {
+    console.log(this.state)
+    const {
+      data: {
+        product: {
+          title,
+          price,
+          image: { fluid },
+          info: { info },
+        },
+      },
+    } = this.props
+    return (
+      <Layout>
+        <div style={{ textAlign: "center" }}>
+          <Link to="/products">Back to Products</Link>
+        </div>
+        <section className="single-product">
+          {/* <Image fluid={fluid} alt={title} /> */}
+          <article>
+            <BackgroundImage
+              style={{
+                backgroundPosition: `center -${this.state.offset / 4}px`,
+              }}
+              fluid={fluid}
+              className="gatsby-background-image"
+            />
+          </article>
+          <article style={{ padding: "2rem" }}>
+            <h1>{title}</h1>
+            <h3>${price}</h3>
+            <p>{info}</p>
+            <button>Add to Cart</button>
+          </article>
+        </section>
+      </Layout>
+    )
+  }
 }
 
 export const query = graphql`
@@ -49,4 +79,4 @@ export const query = graphql`
   }
 `
 
-export default ProductTemplate
+export default productTemplate
